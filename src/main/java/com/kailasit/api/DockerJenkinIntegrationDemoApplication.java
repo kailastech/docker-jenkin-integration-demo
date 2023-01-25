4e985b011e77f6093b3acd46d8c19ae44dc5a8db
kailastech <kailashvannal@gmail.com>
2023-01-25 12:24:29 +0530

    first commit

---
+++ src/main/java/com/kailasit/api/DockerJenkinIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kailasit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kailasit/api/DockerJenkinIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kailasit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
